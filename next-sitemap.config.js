a095c3cfb3eb93cfb4d9a5fb4e4e3303fe9d483c
Jeremiah <jerryedeebass@gmail.com>
2025-09-09 22:07:20 -0700

    Added sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://yourdomain.com', // <-- change to your real domain
+  generateRobotsTxt: true, // also generate robots.txt
+  sitemapSize: 7000, // optional
+};
