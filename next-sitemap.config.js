46c405b86c97985532f2a07a924092f0a8cbd8f8
Jeremiah <jerryedeebass@gmail.com>
2025-09-09 22:17:44 -0700

    fix: update next-sitemap config with cassiebrainz.com

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://yourdomain.com', // <-- change to your real domain
+  siteUrl: 'https://cassiebrainz.com', // <-- change to your real domain
   generateRobotsTxt: true, // also generate robots.txt
   sitemapSize: 7000, // optional
 };
